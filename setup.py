572794591b31787dd5a810887f5725157a880d96
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-08-07 01:42:10 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    name="sb3_support",
+    packages=['sb3_support'],
+    package_dir={'': 'src'},
+
+    description="Stable Baselines3 Support for ROS",
+    url="https://github.com/ncbdrck/ros_rl/",
+    keywords=['ROS', 'reinforcement learning', 'machine-learning', 'gym', 'robotics', 'openai', 'stable-baselines3'],
+
+    author='Jayasekara Kapukotuwa',
+    author_email='j.kapukotuwa@research.ait.ie',
+
+    license="MIT",
+)
+
+setup(**setup_args)
