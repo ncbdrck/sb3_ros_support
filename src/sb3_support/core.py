eebaf079857020ca1e15d41ba4bcf09776882766
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-08-07 19:40:10 +0100

    updated core.py

--- src/sb3_support/core.py
+++ src/sb3_support/core.py
@@ -9,7 +9,7 @@ Recreated to overcome the following errors:
 """
 import os
 from datetime import datetime
-from sb3_support.utils.sb3_common import get_policy_kwargs, get_action_noise
+from sb3_support.utils.sb3_common import get_policy_kwargs, get_action_noise, test_env
 
 # ROS packages required
 import rospy
@@ -23,7 +23,6 @@ from stable_baselines3.common.logger import configure
 
 class BasicModel:
     """
-
     Base class for all the algorithms of Stable Baselines3.
     """
 
@@ -36,7 +35,7 @@ class BasicModel:
             parm_dict (dict): The dictionary containing the parameters.
             log_path (str): The path to save the log.
             load_trained (bool): Whether to load a trained model or not.
-            action_noise_type (str): The type of action noise to use. Can be "normal" or "ornstein".
+            action_noise_type (str): The type of action noise to use. Can be "normal" or "ornstein". (Optional)
         """
 
         self.env = env
@@ -44,6 +43,7 @@ class BasicModel:
         self.log_path = log_path
         self.save_trained_model_path = None
         self.model = None
+        self.parm_dict = parm_dict
 
         if load_trained is False:
             # --- Policy kwargs
@@ -53,23 +53,22 @@ class BasicModel:
             self.action_noise = get_action_noise(self.env.action_space.shape[-1], parm_dict, action_noise_type)
 
             # --- Callback
-            save_freq = rospy.get_param(ns + "/model_params/save_freq")
-            save_prefix = rospy.get_param(ns + "/model_params/save_prefix")
+            save_freq = parm_dict["save_freq"]
+            save_prefix = parm_dict["save_prefix"]
             self.checkpoint_callback = CheckpointCallback(save_freq=save_freq, save_path=save_model_path,
                                                           name_prefix=save_prefix)
 
     def train(self) -> bool:
         """
-        Function to train the model the number of steps specified in the ROS parameter server.
+        Function to train the model the number of steps specified in the yaml config file.
         The function will automatically save the model after training.
 
-        :return: True if the training was successful, False otherwise.
-        :rtype: bool
+        Returns:
+            bool: True if the model was trained, False otherwise.
         """
-
-        training_steps = rospy.get_param(self.ns + "/model_params/training_steps")
-        learn_log_int = rospy.get_param(self.ns + "/model_params/log_interval")
-        learn_reset_num_tm = rospy.get_param(self.ns + "/model_params/reset_num_timesteps")
+        training_steps = self.parm_dict["training_steps"]
+        learn_log_int = self.parm_dict["log_interval"]
+        learn_reset_num_tm = self.parm_dict["reset_num_timesteps"]
 
         if learn_reset_num_tm is False:
             self.env = self.model.get_env()
@@ -86,12 +85,12 @@ class BasicModel:
         """
         Function to save the model.
 
-        :return: True if the model was saved, False otherwise.
-        :rtype: bool
+        Returns:
+            bool: True if the model was saved, False otherwise.
         """
 
         # --- Model name
-        trained_model_name = rospy.get_param(self.ns + "/model_params/trained_model_name")
+        trained_model_name = self.parm_dict["trained_model_name"]
 
         # If file exists, name the new model with a suffix
         self.save_trained_model_path = self.save_model_path + trained_model_name
@@ -106,30 +105,29 @@ class BasicModel:
 
         return True
 
-    def save_replay_buffer(self) -> bool:
+    def save_replay_buffer(self):
         """
         Funtion to save the replay buffer, to be used the training must be finished or an error will be raised.
 
-        :return: True if the replay buffer was saved, False otherwise.
-        :rtype: bool
+        Returns:
+            bool: True if the replay buffer was saved, False otherwise.
         """
 
         if self.save_trained_model_path is None:
             raise ValueError("Model not trained yet, cannot save replay buffer")
 
-        if rospy.get_param(self.ns + "/model_params/save_replay_buffer"):
+        if self.parm_dict["save_replay_buffer"]:
             rospy.logwarn("Saving replay buffer")
             self.model.save_replay_buffer(self.save_trained_model_path + '_replay_buffer')
 
     def set_model_logger(self) -> bool:
         """
-        Function to set the logger of the model.
+        Function to set a logger of the model.
 
-        :return: True if the logger was set, False otherwise.
-        :rtype: bool
+        Returns:
+            bool: True if the logger was set, False otherwise.
         """
-
-        log_folder = rospy.get_param(self.ns + "/model_params/log_folder")
+        log_folder = self.parm_dict["log_folder"]
         log_path = self.log_path + log_folder
         assert not os.path.exists(log_path), "Log folder already exists, to log into that folder first delete it."
         new_logger = configure(log_path + '/', ["stdout", "csv", "tensorboard"])
@@ -141,8 +139,8 @@ class BasicModel:
         """
         Use the env close method to close the environment.
 
-        :return: True if the environment was closed, False otherwise.
-        :rtype: bool
+        Returns:
+            bool: True if the environment was closed, False otherwise.
         """
 
         self.env.close()
@@ -152,31 +150,23 @@ class BasicModel:
         """
         Use the stable-baselines check_env method to check the environment.
 
-        :return: True if the environment is correct, False otherwise.
-        :rtype: bool
+        Returns:
+            bool: True if the environment was checked, False otherwise.
         """
-
-        self.env.check_env()
+        test_env(self.env)
         return True
 
-    def predict(self, observation, state=None, mask=None, deterministic=False):
+    def predict(self, observation, state=None, deterministic=False):
         """
         Get the current action based on the observation, state or mask
 
-        :param observation: The enviroment observation
-        :type observation: ndarray
+        Args:
+            observation (ndarray): The environment observation.
+            state (ndarray): The previous states of the environment, used in recurrent policies. (Optional)
+            deterministic (bool): Whether to return deterministic actions or not. (Optional)
 
-        :param state: The previous states of the enviroment, used in recurrent policies.
-        :type state: ndarray
-
-        :param mask: The mask of the last states, used in recurrent policies.
-        :type mask: ndarray
-
-        :param deterministic: Whether or not to return deterministic actions.
-        :type deterministic: bool
-
-        :return: The action to be taken and the next state(for recurrent policies)
-        :rtype: ndarray, ndarray
+        Returns:
+            ndarray: The action to be taken.
         """
 
         return self.model.predict(observation, state=state, deterministic=deterministic)
