11b043e005fa928684cef4a46de852b7eedc1fa8
Jayasekara Kapukotuwa <ncbdrck@gmail.com>
2023-10-03 10:59:35 +0100

    fixed load_trained_model method

--- src/sb3_ros_support/dqn.py
+++ src/sb3_ros_support/dqn.py
@@ -69,7 +69,7 @@ class DQN(core.BasicModel):
         parm_dict = yaml_utils.load_yaml(pkg_name=config_file_pkg, file_name=config_filename,
                                          file_abs_path=abs_config_path)
 
-        # --- Init super class
+        # --- Init superclass
         super().__init__(env, save_model_path, log_path, parm_dict, load_trained=load_trained)
 
         if load_trained:
@@ -120,7 +120,7 @@ class DQN(core.BasicModel):
                 else:
                     rospy.logwarn("No replay buffer found")
 
-            else:  # Create new model
+            else:  # Create a new model
                 rospy.logwarn("Creating new model")
 
                 self.model = stable_baselines3.DQN("MlpPolicy", env, verbose=1, learning_rate=model_learning_rate,
@@ -138,20 +138,34 @@ class DQN(core.BasicModel):
             # --- Logger
             self.set_model_logger()
 
-
-    def load_trained_model(model_path, model_pkg_path=None, env=None):
+    @staticmethod
+    def load_trained_model(model_path, model_pkg=None, env=None, config_file_pkg=None, config_filename=None,
+                           abs_config_path=None):
         """
         Load a trained model. Use only with predict function, as the logs will not be saved.
 
         Args:
-            model_path (str): The path to the trained model.
-            model_pkg_path (str): The package name to load the model.
+            model_path (str): The path to the trained model. Can be absolute or relative.
+            model_pkg (str): The package name to load the model. Required if abs_model_path is relative.
             env (gym.Env): The environment to be used.
+            config_file_pkg (str): The package name of the config file. Use the same package as model_pkg if not provided.
+            config_filename (str): The name of the config file.
+            abs_config_path (str): The absolute path to the config file.
         Returns:
             model: The loaded model.
         """
 
-        model = DQN(env=env, save_model_path=model_path, log_path=model_path, load_model_path=model_path,
-                    model_pkg_path=model_pkg_path, load_trained=True)
+        if config_file_pkg is None and config_filename is None and abs_config_path is None:
+            config_file_pkg = "sb3_ros_support"
+            config_filename = "dqn.yaml"
+
+            rospy.logwarn("Using default config file: " + config_filename + " from package: " + config_file_pkg)
+
+        elif model_pkg is not None and config_filename is not None and config_file_pkg is None:
+            config_file_pkg = model_pkg
+
+        model = DQN(env=env, save_model_path=model_path, log_path=model_path, model_pkg_path=model_pkg,
+                    load_trained=True, load_model_path=model_path, config_file_pkg=config_file_pkg,
+                    config_filename=config_filename, abs_config_path=abs_config_path)
 
         return model
